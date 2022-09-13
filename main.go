8d07230c85e5ab5cd38252fc359e0598bc9395bd
Asakijz <807480500@qq.com>
2022-09-13 20:51:59 +0800

    匹配前缀返回对应路由

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
+
+// 上述语句用于打包时将指定目录下的文件一并打包
 
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		r := gin.Default()
-		r.GET("/", func(ctx *gin.Context) {
-			ctx.Writer.Write([]byte("test message"))
+		staticFiles, _ := fs.Sub(FS, "frontend/dist") // 将所有文件打包成一个变量
+		// 静态文件都在 /static 这个路由，http.FS 读取文件
+		r.StaticFS("/static", http.FS(staticFiles))
+		r.NoRoute(func(ctx *gin.Context) {
+			path := ctx.Request.URL.Path
+			if strings.HasPrefix(path,"/static/"){
+				reader,err:=staticFiles.Open("index.html")
+				if err!=nil{
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				
+			}
 		})
 		r.Run(":8080")
 	}()
@@ -24,7 +45,7 @@ func main() {
 
 	// 先写死路径开启 chrome
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()
 
 	// 等待退出信号
