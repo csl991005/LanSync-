e6199714827c763afa3f2d8bbe155e70cb49406b
Asakijz <807480500@qq.com>
2022-09-16 00:17:35 +0800

    上传接口-50%
    Took 8 minutes

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"embed"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //go:embed frontend/dist/*
@@ -18,12 +22,47 @@ var FS embed.FS
 
 // 上述语句用于打包时将指定目录下的文件一并打包
 
+func TestController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		exe, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe)
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := uuid.New().String()
+		uploads := filepath.Join(dir, "uploads")
+		err = os.MkdirAll(uploads, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"url": "/" + fullpath,
+		})
+	}
+}
+
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		r := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist") // 将所有文件打包成一个变量
 		// 静态文件都在 /static 这个路由，http.FS 读取文件
+		r.GET("api/v1/texts", TestController)
 		r.StaticFS("/static", http.FS(staticFiles))
 		r.NoRoute(func(ctx *gin.Context) {
 			path := ctx.Request.URL.Path
