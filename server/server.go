3743bdee350b291e48de182b6cb6d44e32603ff5
Asakijz <807480500@qq.com>
2022-09-20 23:35:24 +0800

    refactor

---
+++ server/server.go
@@ -0,0 +1,169 @@
+package server
+
+import (
+	"embed"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/skip2/go-qrcode"
+)
+
+//go:embed frontend/dist/*
+var FS embed.FS
+
+// 上述语句用于打包时将指定目录下的文件一并打包
+
+func Run() {
+	port := "27149"
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	staticFiles, _ := fs.Sub(FS, "frontend/dist") // 将所有文件打包成一个变量
+	// 静态文件都在 /static 这个路由，http.FS 读取文件
+	r.StaticFS("/static", http.FS(staticFiles))
+	r.POST("/api/v1/files", FilesController)
+	r.GET("/api/v1/qrcodes", QrcodesController)
+	r.GET("/uploads/:path", UploadsController)
+	r.GET("/api/v1/addresses", AddressesController)
+	r.POST("/api/v1/texts", TextController)
+	r.NoRoute(func(ctx *gin.Context) {
+		path := ctx.Request.URL.Path
+		if strings.HasPrefix(path, "/static/") {
+			reader, err := staticFiles.Open("index.html")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer reader.Close()
+			stat, err := reader.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+			ctx.DataFromReader(http.StatusOK, stat.Size(), "text/html;charset=utf-8", reader, nil)
+		} else {
+			ctx.Status(http.StatusNotFound)
+		}
+	})
+	r.Run(":" + port)
+}
+
+func AddressesController(c *gin.Context) {
+	add, _ := net.InterfaceAddrs()
+	var result []string
+	for _, address := range add {
+		// 类型断言
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				result = append(result, ipnet.IP.String())
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"addresses": result,
+	})
+}
+
+func GetUploadsDir() (uploads string) {
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	uploads = filepath.Join(dir, "uploads")
+	return
+}
+
+func UploadsController(c *gin.Context) {
+	if path := c.Param("path"); path != "" {
+		target := filepath.Join(GetUploadsDir(), path)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+path)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
+}
+
+func TextController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		exe, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe)
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := uuid.New().String()
+		uploads := filepath.Join(dir, "uploads")
+		err = os.MkdirAll(uploads, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"url": "/" + fullpath,
+		})
+	}
+}
+
+func QrcodesController(c *gin.Context) {
+	if content := c.Query("content"); content != "" {
+		png, err := qrcode.Encode(content, qrcode.Medium, 256)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Data(http.StatusOK, "image/png", png)
+	} else {
+		c.Status(http.StatusBadRequest)
+	}
+}
+
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"url": "/" + fullpath,
+	})
+}
